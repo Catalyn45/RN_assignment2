6833fa0b416e9ed9e8a33f9dc618443b81494ead
Catalyn45 <ancutei.catalin@gmail.com>
2021-10-18 23:12:14 +0300

    test with valid set after each epoch

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import gzip
 import numpy
 import pickle
 from dataclasses import dataclass
+import random
+from scipy.special import softmax
 
 
 @dataclass()
@@ -11,54 +13,46 @@ class Model:
 
 
 def activation(value):
-    return 0 if value < 0 else 1
+    return 0 if value <= 0 else 1
 
 
-def train(model, train_set, learning_rate=0.09, max_iterations=-1, min_success_rate=0.75):
+def train(model, train_set, test_set, learning_rate=0.09, max_iterations=-1):
     images, labels = train_set
+    samples = list(zip(images, labels))
+
     all_classified = False
     iterations = 0
 
     while not all_classified:
-        iterations += 1
-
-        print(f'iteration: {iterations}')
         all_classified = True
 
-        correctly_classified = 0
-        for image_index, (image, label) in enumerate(zip(images, labels)):
-            print(f'images passed: {image_index}') if image_index % 10000 == 0 else None
-
+        random.shuffle(samples)
+        for image_index, (image, label) in enumerate(samples):
             t = numpy.zeros(10)
             t[label] = 1
 
             z = numpy.sum(model.perceptrons * numpy.array(image), axis=1) + model.bias
 
-            output = numpy.vectorize(activation)(z)
+            output = softmax(z) if USE_SOFTMAX else numpy.vectorize(activation)(z)
 
             model.perceptrons = model.perceptrons + numpy.multiply(image.reshape((1, 784)).repeat(10, axis=0),
                                                                    ((t - output) * learning_rate)[:, numpy.newaxis])
             model.bias = model.bias + (t - output) * learning_rate
 
-            if not numpy.array_equal(t, output):
+            if not numpy.argmax(output) == numpy.argmax(t):
                 all_classified = False
-            else:
-                correctly_classified += 1
 
-        success_rate = round(correctly_classified / 50000 * 100)
-        print(
-            f'correctly classified: {correctly_classified} Success rate: {success_rate}%')
-
-        if success_rate / 100 >= min_success_rate:
-            break
+        test(model, test_set, iterations)
 
         if max_iterations != -1 and iterations >= max_iterations:
             break
 
+        iterations += 1
+
     print("Training complete")
 
 
-def test(model, test_set):
+def test(model, test_set, epoch):
     success = 0
     failed = 0
 
@@ -67,22 +61,14 @@ def test(model, test_set):
     for image_index, (image, label) in enumerate(zip(test_images, test_labels)):
         z = numpy.sum(model.perceptrons * image, axis=1) + model.bias
 
-        outputs = numpy.vectorize(activation)(z)
+        outputs = softmax(z) if USE_SOFTMAX else numpy.vectorize(activation)(z)
 
-        numbers = numpy.where(outputs == 1)[0]
-
-        max_chance = None
-
-        for number in numbers:
-            if max_chance is None or z[number] > z[max_chance]:
-                max_chance = number
-
-        if max_chance == label:
+        if numpy.argmax(outputs) == label:
             success += 1
         else:
             failed += 1
 
-    print(f'Success rate: {round(success / (success + failed) * 100)}%')
+    print(f'Epoch: {epoch} success rate: {round(success / (success + failed) * 100)}%')
 
 
 def get_datasets(file_name):
@@ -90,13 +76,15 @@ def get_datasets(file_name):
         return pickle.load(file, encoding='latin')
 
 
+USE_SOFTMAX = False
+
+
 def main():
     train_set, valid_set, test_set = get_datasets("mnist.pkl.gz")
 
     model = Model()
 
-    train(model, train_set, learning_rate=0.01, max_iterations=50, min_success_rate=0.8)
-    test(model, valid_set)
+    train(model, train_set, valid_set, learning_rate=0.003, max_iterations=50)
 
 
 if __name__ == '__main__':
